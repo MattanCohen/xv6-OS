f2fd03f37b52b63cf5c603d55c1d2f65f41732f2
NoaLevin2710 <117371582+NoaLevin2710@users.noreply.github.com>
2023-03-21 21:13:20 +0000

    better and cooler hello

--- user/hello.c
+++ user/hello.c
@@ -2,18 +2,27 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
+void sendOutput(char* message){
+  write(1, message, strlen(message));
+}
+
+char* getInput(){
+  char * inputMessage = malloc(sizeof(char) * 60);
+  gets(inputMessage, sizeof(inputMessage));
+  return inputMessage;
+}
+
 int
 main(int argc, char *argv[])
 {
-  int i;
-
-  for(i = 1; i < argc; i++){
-    write(1, argv[i], strlen(argv[i]));
-    if(i + 1 < argc){
-      write(1, " ", 1);
-    } else {
-      write(1, "\n", 1);
-    }
+  
+  char* inputMessage;
+  sendOutput("Hello how can I help you?\n");
+  
+  while((strcmp((inputMessage = getInput()), "q\n") != 0)  && (strcmp(inputMessage, "Q\n") != 0)){
+    sendOutput(inputMessage);
   }
+
   exit(0);
 }
